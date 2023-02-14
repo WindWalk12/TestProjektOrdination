3b5bf97079cc4826da4661cd987c904cd109ab53
Maksim <lavrinenkom03@gmail.com>
2023-02-14 11:52:26 +0100

    Fixet DagligFast methods

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -16,16 +16,16 @@ public class DagligFast extends Ordination {
     public DagligFast(LocalDate startDate, LocalDate slutDate, Patient patient, Laegemiddel laegemiddel, double morgenAntal, double middagAntal,
                       double aftenAntal, double natAntal) {
         super(startDate, slutDate, patient, laegemiddel);
-        if (morgenAntal>0) {
+        if (morgenAntal>=0) {
             dosisArray[0] = new Dosis(LocalTime.of(9,0), morgenAntal);
         }
-        if (middagAntal>0) {
+        if (middagAntal>=0) {
             dosisArray[1] = new Dosis(LocalTime.of(12,0), middagAntal);
         }
-        if (aftenAntal>0) {
+        if (aftenAntal>=0) {
             dosisArray[2] = new Dosis(LocalTime.of(18,0), aftenAntal);
         }
-        if (natAntal>0) {
+        if (natAntal>=0) {
             dosisArray[3] = new Dosis(LocalTime.of(21,0), natAntal);
         }
     }
@@ -33,19 +33,19 @@ public class DagligFast extends Ordination {
     @Override
     public double samletDosis() {
         if ((dosisArray[0].getAntal() + dosisArray[1].getAntal() +
-                dosisArray[2].getAntal() + dosisArray[3].getAntal()) < 5) {
+                dosisArray[2].getAntal() + dosisArray[3].getAntal()) > 4) {
             throw new RuntimeException("Samlet antal dosis må være 4 højest");
         }
         double antalDosis = dosisArray[0].getAntal() + dosisArray[1].getAntal() +
                 dosisArray[2].getAntal() + dosisArray[3].getAntal();
-        long daysBetween = ChronoUnit.DAYS.between(getSlutDen(), getStartDen()) + 1;
+        long daysBetween = ChronoUnit.DAYS.between(getStartDen(), getSlutDen()) + 1;
         return daysBetween * antalDosis;
     }
 
     @Override
     public double doegnDosis() {
         if ((dosisArray[0].getAntal() + dosisArray[1].getAntal() +
-                dosisArray[2].getAntal() + dosisArray[3].getAntal()) < 5) {
+                dosisArray[2].getAntal() + dosisArray[3].getAntal()) > 4) {
             throw new RuntimeException("Samlet antal dosis må være 4 højest");
         }
         return dosisArray[0].getAntal() + dosisArray[1].getAntal() + dosisArray[2].getAntal() + dosisArray[3].getAntal();
