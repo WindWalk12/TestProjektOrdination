987f3a67f34607d1961c9069f8218a364ac985c9
Maksim <lavrinenkom03@gmail.com>
2023-02-14 09:49:26 +0100

    Lavat class DagligFast

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,43 @@
 package ordination;
 
-public class DagligFast {
-    // TODO
+import gui.TypeOrdination;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+
+public class DagligFast extends Ordination {
+    //TODO
+
+    private Dosis[] dosisArray = new Dosis[4];
+
+
+    public DagligFast(LocalDate startDate, LocalDate slutDate, TypeOrdination type, Dosis[] dosisArray) {
+        super(startDate, slutDate, type);
+        this.dosisArray = dosisArray;
+    }
+
+    @Override
+    public double samletDosis() {
+        double antalDosis = dosisArray[0].getAntal() + dosisArray[1].getAntal() +
+                dosisArray[2].getAntal() + dosisArray[3].getAntal();
+        long daysBetween = ChronoUnit.DAYS.between(getSlutDen(), getStartDen());
+        return daysBetween * antalDosis;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return dosisArray[0].getAntal() + dosisArray[1].getAntal() + dosisArray[2].getAntal() + dosisArray[3].getAntal();
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+
+    public Dosis[] getDosisArray() {
+        return dosisArray;
+    }
+
 }
