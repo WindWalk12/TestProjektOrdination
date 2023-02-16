c8a8124c30be97740fcec25218e333737362ff63
WindWalk <nhygta@hotmail.com>
2023-02-16 08:42:40 +0100

    Oprettet test source folder

---
+++ tests/ordination/PNTest.java
@@ -0,0 +1,19 @@
+package ordination;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PNTest {
+
+    @Test
+    void givDosis() {
+    }
+
+    @Test
+    void doegnDosis() {
+    }
+
+    @Test
+    void samletDosis() {
+    }
+}
